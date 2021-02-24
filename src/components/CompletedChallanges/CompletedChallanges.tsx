7133876d58d798074c8f80aba88491814da96bff
William Takayama <william-sobreira@hotmail.com>
2021-02-24 20:41:49 -0300

    (Feat) component of completed challenges only style

---
+++ src/components/CompletedChallanges/CompletedChallanges.tsx
@@ -0,0 +1,10 @@
+import classes from './CompletedChallanges.module.scss';
+
+export function CompletedChallanges() {
+  return (
+    <div className={classes.container}>
+      <span>Completed Challenges</span>
+      <span>5</span>
+    </div>
+  );
+}
