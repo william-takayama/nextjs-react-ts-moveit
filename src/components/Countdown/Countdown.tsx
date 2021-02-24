85202e737847f4c0a04b874aeed4368a3a1b9539
William Takayama <william-sobreira@hotmail.com>
2021-02-24 20:42:10 -0300

    (Feat) Simple Countdown using useEffect

---
+++ src/components/Countdown/Countdown.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import classes from './Countdown.module.scss';
+
+export function Countdown() {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minLeft, minRight] = String(minutes).padStart(2, '0').split('');
+  const [secondsLeft, secondsRight] = String(seconds)
+    .padStart(2, '0')
+    .split('');
+
+  function startCountDown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
+  return (
+    <div>
+      <div className={classes.container}>
+        <div>
+          <span>{minLeft}</span>
+          <span>{minRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
+        </div>
+      </div>
+
+      <button type="button" className={classes.button} onClick={startCountDown}>
+        Initialize cycle
+      </button>
+    </div>
+  );
+}
