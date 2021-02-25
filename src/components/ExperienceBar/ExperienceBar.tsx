99965ef5a16a8a72232de95f4a7427619346999e
William Takayama <william-sobreira@hotmail.com>
2021-02-24 21:18:48 -0300

    (Update) styles

--- src/components/ExperienceBar/ExperienceBar.tsx
+++ src/components/ExperienceBar/ExperienceBar.tsx
@@ -1,12 +1,12 @@
-import classes from "./ExperienceBar.module.scss";
+import classes from './ExperienceBar.module.scss';
 
 export function ExperienceBar() {
   return (
     <header className={classes.header}>
       <span>0 xp</span>
       <div>
-        <div style={{ width: "60%" }} />
-        <span className={classes.currentExperience} style={{ left: "50%" }}>
+        <div style={{ width: '50%' }} />
+        <span className={classes.currentExperience} style={{ left: '50%' }}>
           300xp
         </span>
       </div>
