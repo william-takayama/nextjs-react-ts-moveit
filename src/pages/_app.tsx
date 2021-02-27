77ce33d669e00df6d7ae8c205a20a23e6534925f
William Takayama <william-sobreira@hotmail.com>
2021-02-27 19:26:15 -0300

    (Fix) tipagem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { ChallengesProvider } from '../contexts/ChallengesContext';
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }) {
+interface MyAppProps {
+  Component: React.FunctionComponent;
+  pageProps: object;
+}
+
+function MyApp({ Component, pageProps }: MyAppProps): JSX.Element {
   return (
     <ChallengesProvider>
       <Component {...pageProps} />
