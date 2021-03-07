ef4423b0831d2cbfd2b8b6abb87dd8f38c438946
William Takayama <william-sobreira@hotmail.com>
2021-03-07 12:47:07 -0300

    (Update) layout placeholder component

--- src/components/Placeholder/Placeholder.tsx
+++ src/components/Placeholder/Placeholder.tsx
@@ -1,4 +1,6 @@
 import { BaseProps } from '../../interfaces/BaseProps';
+import classes from './Placeholder.module.scss';
+import cn from 'clsx';
 
 interface PlaceholdeProps extends BaseProps {
   width?: string | number;
@@ -9,15 +11,17 @@ interface PlaceholdeProps extends BaseProps {
 
 export default function Placeholder({
   id,
-  width = '100%',
-  height = '100%',
+  width,
+  height,
   borderRadius = 'none',
-  backgroundColor = 'var(--color-positive)',
+  backgroundColor = 'var(--color-xx-contrast)',
+  className,
   style,
 }: PlaceholdeProps): JSX.Element {
   return (
     <div
       id={id}
+      className={cn(className, classes.placeholder)}
       style={{
         backgroundColor: backgroundColor,
         width: width,
